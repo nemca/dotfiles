918f0c4c5864de8eb2856beac7058a59cce7f934
Michael Bruskov <mixanemca@yandex.ru>
2017-01-25 12:11:48 -0400

    Add colors settings to config.fish

--- config.fish
+++ config.fish
@@ -29,8 +29,27 @@ set --export HISTTIMEFORMAT '%F %T '
 set --export LC_ALL en_US.UTF-8
 set --export LANG en_US.UTF-8
 
-set fish_color_search_match --background=blue # default --background=purple
-set fish_color_selection --background=grey    # default --background=purple
+# Set colors
+set fish_color_autosuggestion "555 yellow"
+set fish_color_command white
+set fish_color_comment red
+set fish_color_cwd 00875f
+set fish_color_cwd_root red
+set fish_color_end bcbcbc
+set fish_color_error red --bold
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host cyan
+set fish_color_match cyan
+set fish_color_normal 5f5f5f
+set fish_color_operator white
+set fish_color_param normal
+set fish_color_quote brown
+set fish_color_redirection normal
+set fish_color_search_match --background=blue
+set fish_color_selection --background=grey
+set fish_color_status red
+set fish_color_user cyan
 
 # frees up ctrl-s and ctrl-q
 stty -ixon -ixoff
