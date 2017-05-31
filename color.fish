cb2cfde484b0619ac7dda2fc969ad25fd4e82658
Michael Bruskov <mixanemca@yandex.ru>
2017-05-31 14:37:05 +0300

    Add color.fish

---
+++ color.fish
@@ -0,0 +1,20 @@
+set fish_color_autosuggestion 555 yellow
+set fish_color_command white
+set fish_color_comment red
+set fish_color_cwd 00875f
+set fish_color_cwd_root red
+set fish_color_end bcbcbc
+set fish_color_error red --bold
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host cyan
+set fish_color_match cyan
+set fish_color_normal 5f5f5f
+set fish_color_operator white
+set fish_color_param normal
+set fish_color_quote brown
+set fish_color_redirection normal
+set fish_color_search_match --background=blue
+set fish_color_selection --background=grey
+set fish_color_status red
+set fish_color_user cyan
